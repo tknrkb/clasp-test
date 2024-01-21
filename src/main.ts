18cf7f3e254ff5f89df9027bc62302a66a0cd4e7
Takenori-KUBO <tknrkb@gmail.com>
2024-01-22 01:48:10 +0900

    add sample code with google.script.run.

--- src/main.ts
+++ src/main.ts
@@ -4,3 +4,15 @@ function myFunction() {
     Logger.log("hello world from editor");
     Logger.log("hello world from editor");
 }
+
+function clickTEST() {
+    Logger.log('click test');
+    return 1;
+}
+
+function clickTEST2() {
+    Logger.log('click test2');
+    const a = [];
+    throw new Error('Test Error');
+    return a[0];
+}
